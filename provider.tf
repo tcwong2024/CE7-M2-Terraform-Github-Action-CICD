01cac79807cb013c2834bd6de030d7ae0dac5c9d
tcwong2024 <tcwong99@hotmail.com>
2024-09-03 23:05:36 +0800

    update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,7 +17,17 @@ provider "aws" {
   region = var.region_name
 }
 
+resource "aws_kms_key" "sns_key" {
+  description = "KMS key for SNS topic encryption"
+}
+
+resource "aws_kms_alias" "sns_key_alias" {
+  name          = "alias/sns-key"
+  target_key_id = aws_kms_key.sns_key.id
+}
+
 resource "aws_sns_topic" "user_updates" {
   name              = var.sns_topic_name
-  kms_master_key_id = "alias/aws/sns" # Enable encryption using the default KMS key
+  # kms_master_key_id = "alias/aws/sns" # Enable encryption using the default KMS key
+  kms_master_key_id = aws_kms_key.sns_key.arn
 }
